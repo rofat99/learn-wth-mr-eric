d1abc1a9c5823d025c719be4b942b1e851650a09
Arofat <rofat99@gmail.com>
2024-02-01 01:04:03 +0700

    init

---
+++ src/test/java/com/pm/eric/learning/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.eric.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
